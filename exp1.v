05cc1625e07a5b9e641a0e8b4fa76a8e7832dce8
Mai Ziyang <66940944+maizhouzi@users.noreply.github.com>
2022-10-11 16:42:55 +0800

    1

---
+++ exp1.v
@@ -0,0 +1,25 @@
+module(
+    input clk,
+    input[3:0] A,
+    input[3:0] B,
+    output[4:0] OUT,
+    output reg[4:0] COUNT
+    );
+    assign OUT=A+B;
+    always@(posedge clk)
+    begin
+        if (OUT==5'b10000)
+        begin
+            COUNT=COUNT+1;
+        end
+        else begin
+            COUNT=COUNT;
+        end
+    end
+
+
+
+
+endmodule
+
+
